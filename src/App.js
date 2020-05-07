10b48dd37ed6ebb6dd239087c7cadc417c13666b
AlinA414 <62790604+AlinA414@users.noreply.github.com>
2020-05-07 13:07:09 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const val = 'я пересменная'
+const logic = true;
+const tern = false;
+
 function App() {
   return (
     <div className="App">
@@ -10,7 +14,13 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Hello World!</p>
+        <p style={{color: 'red'}}>Hello World!</p>
+    <p>Вывод значения переменной: {val}</p>
+    <p>Вывод числа: {55}</p>
+    <p>Вывоа результата арифметической операции: {5 + 5}</p>
+    <p>Вывод результата логической операции: {logic && <span>Условие истинно))</span}</p>
+    <p>Вспользование тернарного условия: {tern ? <span>Условие истинно))</span> : <span>Все плохо((</span>}</p>
+    <p>Вывод undefined: {undefined}, null: {null}, false: {false}, true: {true}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
